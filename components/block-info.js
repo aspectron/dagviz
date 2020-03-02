82b91e9d6c15debea20da893bb33c4137932c944
surinder singh <surinder83singh@gmail.com>
2020-03-02 16:14:13 +0530

    Update block-info.js

--- components/block-info.js
+++ components/block-info.js
@@ -119,7 +119,7 @@ class BlockInfo extends BaseElement{
                         <div @click="${this.focusClick}" class="button" style="background-image:url(/resources/images/icons/geo-fence.png);transform:scale(1.1); opacity:0.75;" tooltip="fal fa-map-marker-alt:Go to block ${data.blockHash.substring(0,18)+'...'}"></div>
                         <div style="flex:1;min-width:16px;"></div>
                         <div @click="${this.copyLinkToClipboard}" class="button" style="background-image:url(/resources/images/icons/copy-link-2.png);transform:scale(1.1);opacity:0.75;" tooltip="fa-link:Copy link to clipboard (for block ${data.blockHash.substring(0,18)+'...'} only)"></div>
-                        <div @click="${this.close}" class="button" x-tooltip="Close" 
+                        <div class="button close-btn" x-tooltip="Close" 
                             style="background-image:url(/resources/images/icons/cross.png);position:relative;transform:scale(0.85) translate(14px,-8px);"></div>
                     </div>
                     <div class='info-basic'>
@@ -175,7 +175,10 @@ class BlockInfo extends BaseElement{
     click(e) {
         console.log('click!',e);
     }
-    panelClick() {
+    panelClick(e) {
+        if(e.target.classList.contains('close-btn'))
+            return this.close();
+
         if($(this.el).hasClass('advanced')) {
             // $(this.el).toggleClass('advanced');
             // $("block-info").css('display','block');
